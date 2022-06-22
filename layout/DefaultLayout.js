0b53a7e313b4e3c1dd4ff6afa50ef4924fc7a7a0
Purinat Sanbundit <fame3js49@gmail.com>
2022-06-22 18:10:47 +0700

    feat: finished base layout

--- layout/DefaultLayout.js
+++ layout/DefaultLayout.js
@@ -3,9 +3,21 @@ import React from "react"
 export default function DefaultLayout(props) {
   return (
     <>
-      <nav>Nav</nav>
-      <main className="page-container">{props.children}</main>
-      <footer></footer>
+      <nav>
+        <h1 className="p-4 mx-auto max-w-lg w-full text-center uppercase text-gray-700 text-xl tracking-widest">
+          My Dashboard
+        </h1>
+      </nav>
+      <main>{props.children}</main>
+      <footer className="p-4 mx-auto w-full text-right absolute bottom-0 z-50">
+        <a
+          href="https://chofongsua-blog.netlify.app/"
+          className="text-sm underline text-gray-700"
+          title="Purinat Sanbundit Profile's website"
+        >
+          by Purinat Sanbundit
+        </a>
+      </footer>
     </>
   )
 }
